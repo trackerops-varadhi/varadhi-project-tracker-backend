89b34f1e47ec10e48329f8a990b3503b5d1f9a7e
shiva sai <shivasaia55@gmail.com>
2026-08-20 21:25:51 +0530

    chnages in cntroller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,7 +2,7 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const pool = require('../config/db')
 const { successResponse, errorResponse } = require('../utils/response')
-
+ 
 const signToken = (id) => jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES_IN || '7d' })
 
 exports.register = async (req, res) => {
